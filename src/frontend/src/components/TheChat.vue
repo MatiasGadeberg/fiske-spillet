40b94cc1c3a506f6308a8dd4d731ce59703c8f50
Matias Gadeberg <m.n.gadeberg@gmail.com>
2023-07-04 19:26:31 +0200

    refactor: Updated to script setup syntax removed css

--- src/frontend/src/components/TheChat.vue
+++ src/frontend/src/components/TheChat.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h1>Time on server is {{ gameTime }}</h1>
     <h1>Write you message here!</h1>
     <ul>
       <li v-for="message in chatMessages" :key="message.id">
@@ -12,52 +13,57 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { MqttWrapper } from '../../../shared/classes/MqttWrapper.js'
+import type { GameInfo } from '../../../shared/types/GameTypes'
 
-export default {
-  name: 'ChatComponent',
-  setup() {
-    const chatMessages = ref([] as { id: number; content: string }[])
-    const inputMessage = ref('')
-    let mqtt: MqttWrapper
+const chatMessages = ref([] as { id: number; content: string }[])
+const inputMessage = ref('')
+const gameTime = ref('')
+let mqtt: MqttWrapper
 
-    const onConnet = (): void => {
-      console.log('Client connected')
-      mqtt.subscribeToTopic('chat/message')
-    }
+const onConnet = (): void => {
+  console.log('Client connected')
+  mqtt.subscribeToTopic('game-data')
+}
 
-    const onMessage = (topic: string, message: any): void => {
-      chatMessages.value.push({ id: Date.now(), content: message.toString() })
-    }
+const onMessage = (topic: string, message: any): void => {
+  console.log(`Recieved message on topic ${topic}:`)
+  console.log(JSON.parse(message))
+  switch (topic) {
+    case 'game-data':
+      updateGameData(JSON.parse(message) as GameInfo)
 
-    const connectToBroker = () => {
-      mqtt = new MqttWrapper({
-        connectionHost: 'localhost',
-        connectionType: 'web-mqtt',
-        onConnectCallbackFunction: onConnet,
-        onMessageCallbackFunction: onMessage
-      })
-    }
+      break
 
-    const sendMessage = () => {
-      const message = inputMessage.value.trim()
-      if (message) {
-        mqtt.publishToTopic('chat/message', message)
-        inputMessage.value = ''
-      }
-    }
-
-    onMounted(() => {
-      connectToBroker()
-    })
-
-    return {
-      chatMessages,
-      inputMessage,
-      sendMessage
-    }
+    default:
+      break
   }
 }
+
+const connectToBroker = () => {
+  mqtt = new MqttWrapper({
+    connectionHost: 'localhost',
+    connectionType: 'web-mqtt',
+    onConnectCallbackFunction: onConnet,
+    onMessageCallbackFunction: onMessage
+  })
+}
+
+const sendMessage = () => {
+  const message = inputMessage.value.trim()
+  if (message) {
+    mqtt.publishToTopic('chat/message', message)
+    inputMessage.value = ''
+  }
+}
+
+const updateGameData = (data: GameInfo): void => {
+  gameTime.value = new Date(data.serverTime).toLocaleTimeString()
+}
+
+onMounted(() => {
+  connectToBroker()
+})
 </script>
