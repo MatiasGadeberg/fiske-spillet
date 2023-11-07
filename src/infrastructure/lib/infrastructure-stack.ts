85c6281a4d3c07afcab8741b742387b2d8c56785
Matias Gadeberg <m.n.gadeberg@gmail.com>
2023-11-07 22:20:37 +0100

    feat: Added infrastructure deployment of backend container

--- src/infrastructure/lib/infrastructure-stack.ts
+++ src/infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,39 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecsP from "aws-cdk-lib/aws-ecs-patterns";
+import path = require("path");
 
 export class InfrastructureStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // The code that defines your stack goes here
+        const cluster = new ecs.Cluster(this, "fiskespil-cluster", {
+            clusterName: "Fiskespillet",
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const taskDefinition = new ecs.FargateTaskDefinition(this, "fiskespil-taks-definition");
+
+        const backendImage = new ecs.AssetImage(path.join(__dirname, "../.."), {
+            file: "backend/Dockerfile.prod",
+            assetName: "fiskespil/backend",
+            exclude: ["infrastructure"],
+        });
+
+        taskDefinition.addContainer("backend", {
+            image: backendImage,
+            containerName: "fiskespil-backend",
+            logging: ecs.LogDrivers.awsLogs({
+                streamPrefix: "fiskespil-backend",
+                mode: ecs.AwsLogDriverMode.NON_BLOCKING,
+                maxBufferSize: cdk.Size.mebibytes(25),
+            }),
+        });
+
+        new ecs.FargateService(this, "fiskespil-service", {
+            cluster,
+            taskDefinition,
+        });
+    }
 }
