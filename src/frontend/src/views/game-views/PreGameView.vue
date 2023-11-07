4ca45c693a9a55f5981658af2c1956cb5238642d
Matias Gadeberg <m.n.gadeberg@gmail.com>
2023-11-07 20:16:09 +0100

    feat: added placeholder files for pregame view and fish store view

---
+++ src/frontend/src/views/game-views/PreGameView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useGameStore } from '@/stores/game'
+const game = useGameStore()
+</script>
+
+<template>
+  <div>
+    <h1>This is the pre game page</h1>
+    <h2>Spillet er ikke startet endnu. Det starter om</h2>
+    <h2>{{ new Date(game.timeToStartInMs).toISOString().slice(11, 19) }}</h2>
+  </div>
+</template>
+
+<style scoped></style>
