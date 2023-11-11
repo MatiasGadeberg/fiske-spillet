3a4cfea18e8a4fbeebcab75fbe3c574a3540dd89
Matias Gadeberg <m.n.gadeberg@gmail.com>
2023-11-11 20:47:01 +0100

    feat: added frontend container to deployment2

---
+++ src/infrastructure/lib/constructs/fiskeService.ts
@@ -0,0 +1,44 @@
+import { Construct } from "constructs";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as cdk from "aws-cdk-lib";
+import path = require("path");
+
+export type FiskeServiceProps = {
+    cluster: ecs.Cluster;
+    infrastructureElemet: "frontend" | "backend";
+    portMappings: ecs.PortMapping[];
+};
+
+export class FiskeService extends Construct {
+    public service: ecs.FargateService;
+
+    constructor(scope: Construct, id: string, props: FiskeServiceProps) {
+        super(scope, id);
+
+        const infraElement = props.infrastructureElemet;
+
+        const taskDefinition = new ecs.FargateTaskDefinition(this, `fiskespil-${infraElement}-taks-definition`);
+
+        const Image = new ecs.AssetImage(path.join(__dirname, "../../.."), {
+            file: `${infraElement}/Dockerfile.prod`,
+            assetName: `fiskespil/${infraElement}`,
+            exclude: ["infrastructure"],
+        });
+
+        taskDefinition.addContainer(`${infraElement}`, {
+            image: Image,
+            containerName: `fiskespil-${infraElement}`,
+            logging: ecs.LogDrivers.awsLogs({
+                streamPrefix: `fiskespil-${infraElement}`,
+                mode: ecs.AwsLogDriverMode.NON_BLOCKING,
+                maxBufferSize: cdk.Size.mebibytes(25),
+            }),
+            portMappings: props.portMappings,
+        });
+
+        this.service = new ecs.FargateService(this, `fiskespil-${infraElement}-service`, {
+            cluster: props.cluster,
+            taskDefinition,
+        });
+    }
+}
--- src/infrastructure/lib/infrastructure-stack.ts
+++ src/infrastructure/lib/infrastructure-stack.ts
@@ -1,39 +1,61 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ecs from "aws-cdk-lib/aws-ecs";
-import * as ecsP from "aws-cdk-lib/aws-ecs-patterns";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import path = require("path");
+import { FiskeService } from "./constructs/fiskeService";
 
 export class InfrastructureStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
+        const vpc = new ec2.Vpc(this, "fiske-vpc", {
+            vpcName: "Fiskenettet",
+            maxAzs: 2,
+        });
+
         // The code that defines your stack goes here
         const cluster = new ecs.Cluster(this, "fiskespil-cluster", {
             clusterName: "Fiskespillet",
+            vpc,
         });
 
-        const taskDefinition = new ecs.FargateTaskDefinition(this, "fiskespil-taks-definition");
-
-        const backendImage = new ecs.AssetImage(path.join(__dirname, "../.."), {
-            file: "backend/Dockerfile.prod",
-            assetName: "fiskespil/backend",
-            exclude: ["infrastructure"],
-        });
-
-        taskDefinition.addContainer("backend", {
-            image: backendImage,
-            containerName: "fiskespil-backend",
-            logging: ecs.LogDrivers.awsLogs({
-                streamPrefix: "fiskespil-backend",
-                mode: ecs.AwsLogDriverMode.NON_BLOCKING,
-                maxBufferSize: cdk.Size.mebibytes(25),
-            }),
-        });
-
-        new ecs.FargateService(this, "fiskespil-service", {
+        const backendService = new FiskeService(this, "fiske-backed", {
             cluster,
-            taskDefinition,
+            infrastructureElemet: "backend",
+            portMappings: [],
+        });
+
+        const frontendService = new FiskeService(this, "fiske-frontend", {
+            cluster,
+            infrastructureElemet: "frontend",
+            portMappings: [
+                {
+                    containerPort: 80,
+                    hostPort: 80,
+                },
+            ],
+        });
+
+        const alb = new elbv2.ApplicationLoadBalancer(this, "MyALB", {
+            vpc,
+            internetFacing: true, // Set to false if internal facing
+        });
+
+        // Create a listener for the ALB
+        const listener = alb.addListener("MyListener", {
+            port: 80, // Adjust the port as needed
+            open: true,
+        });
+
+        // Add the ECS service as a target to the ALB listener
+        listener.addTargets("EcsTargets", {
+            targets: [frontendService.service],
+            healthCheck: {
+                path: "/", // Adjust the path as needed
+            },
+            port: 80,
         });
     }
 }
