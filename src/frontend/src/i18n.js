e8e369d7743880eee2cd75910ea84cd379dae4bf
Matias Gadeberg <m.n.gadeberg@gmail.com>
2023-12-11 22:51:09 +0100

    feat: Added localization initial

--- src/frontend/src/i18n.js
+++ src/frontend/src/i18n.js
@@ -7,14 +7,19 @@ import { createI18n } from 'vue-i18n'
  * See: https://github.com/intlify/vue-i18n-loader#rocket-i18n-resource-pre-compilation
  */
 function loadLocaleMessages() {
-  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-  const messages = {}
-  locales.keys().forEach(key => {
+    const locales = import.meta.glob('./locales/*.json')
+    const messages = {}
+
+  Object.keys(locales).forEach(key => {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i)
     if (matched && matched.length > 1) {
       const locale = matched[1]
-      messages[locale] = locales(key).default
+        locales[key]().then((localeMsg) => {
+            messages[locale] = localeMsg.default
+        })
+      
     }
+    locales[key]()
   })
   return messages
 }
